ae2047b2721f952f73eb3187185720f215e25c7c
Solomon <solomon.oduniyi@gmail.com>
2023-12-22 21:02:25 +0100

    Impl

--- app/src/main/java/com/example/movieappcompose/ui/screen/ScreenRoute.kt
+++ app/src/main/java/com/example/movieappcompose/ui/screen/ScreenRoute.kt
@@ -5,4 +5,6 @@ sealed class ScreenRoute(val route: String) {
     object Home: ScreenRoute(route = "moviesHome")
     object Explore: ScreenRoute(route = "explorerMoviesList/{$ID}")
     object Details: ScreenRoute(route = "movieDetails/{movies_id}")
+
+
 }
