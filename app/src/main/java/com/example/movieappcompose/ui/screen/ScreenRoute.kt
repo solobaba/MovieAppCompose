0e652a678fda4b8d4636ca625b0b727a6133529e
Solomon.Oduniyi <solomon.oduniyi@gmail.com>
2024-01-15 17:07:47 +0100

    Detail explore page

--- app/src/main/java/com/example/movieappcompose/ui/screen/ScreenRoute.kt
+++ app/src/main/java/com/example/movieappcompose/ui/screen/ScreenRoute.kt
@@ -5,4 +5,5 @@ sealed class ScreenRoute(val route: String) {
     object Home: ScreenRoute(route = "moviesHome")
     object Explore: ScreenRoute(route = "explorerMoviesList/{$ID}")
     object Details: ScreenRoute(route = "movieDetails/{movies_id}")
+    object DetailExplore: ScreenRoute(route = "exploreDetails/{movies_id}")
 }
