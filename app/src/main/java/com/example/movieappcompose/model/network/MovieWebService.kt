539a6c89ab122ef817c9fb3dc0f04a7ff46b28ce
Solomon <solomon.oduniyi@gmail.com>
2024-01-26 16:37:41 +0100

    Increase readTimeout and connectTimeout speed

--- app/src/main/java/com/example/movieappcompose/model/network/MovieWebService.kt
+++ app/src/main/java/com/example/movieappcompose/model/network/MovieWebService.kt
@@ -38,8 +38,8 @@ class MovieWebService {
             level = HttpLoggingInterceptor.Level.BODY
         }
         return OkHttpClient.Builder()
-            .readTimeout(200, TimeUnit.SECONDS)
-            .connectTimeout(200, TimeUnit.SECONDS)
+            .readTimeout(250, TimeUnit.SECONDS)
+            .connectTimeout(250, TimeUnit.SECONDS)
             .addInterceptor(loggingInterceptor)
             .build()
     }
