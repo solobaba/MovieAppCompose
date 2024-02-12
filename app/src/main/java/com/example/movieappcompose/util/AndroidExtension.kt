47ce5e17c13b0eefb49ca975e174602a6552dcc4
Solomon <solomon.oduniyi@gmail.com>
2024-02-12 16:54:32 +0100

    Implementing snackbar flow across the codebase

--- app/src/main/java/com/example/movieappcompose/util/AndroidExtension.kt
+++ app/src/main/java/com/example/movieappcompose/util/AndroidExtension.kt
@@ -14,27 +14,3 @@ fun <A> String.fromJson(type: Class<A>): A {
 fun <A> A.toJson(): String? {
     return Gson().toJson(this)
 }
-
-//@OptIn(ExperimentalMaterial3Api::class)
-//@Composable
-//fun SnackBarDemo() {
-//    val scaffoldState: ScaffoldState = rememberScaffoldState()
-//    val coroutineScope: CoroutineScope = rememberCoroutineScope()
-//
-//    Scaffold(scaffoldState = scaffoldState) {
-//        Button(onClick = {
-//            coroutineScope.launch {
-//                val snackbarResult = scaffoldState.snackbarHostState.showSnackbar(
-//                    message = msg,
-//                    actionLabel = "Do something"
-//                )
-//                when (snackbarResult) {
-//                    SnackbarResult.Dismissed -> TODO()
-//                    SnackbarResult.ActionPerformed -> TODO()
-//                }
-//            }
-//        }) {
-//            Text(text = "Click me!")
-//        }
-//    }
-//}
