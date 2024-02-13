7b4908c415c3f5d671088dab21bcd8e4092c6163
Solomon <solomon.oduniyi@gmail.com>
2024-02-13 16:45:36 +0100

    Implemented access to short and long toast across the codebase

--- app/src/main/java/com/example/movieappcompose/util/AndroidExtension.kt
+++ app/src/main/java/com/example/movieappcompose/util/AndroidExtension.kt
@@ -1,6 +1,8 @@
 package com.example.movieappcompose.util
 
+import android.content.Context
 import android.util.Log
+import android.widget.Toast
 import com.google.gson.Gson
 
 fun logTrace(text: String) =  Log.d("Smartx", text)
@@ -13,4 +15,19 @@ fun <A> String.fromJson(type: Class<A>): A {
 //Object To Json String
 fun <A> A.toJson(): String? {
     return Gson().toJson(this)
+}
+
+/**
+ * Shows a short Toast with a String Parameter.
+ */
+fun Context.shortToast(msg: String) {
+    Toast.makeText(this, msg, Toast.LENGTH_SHORT).show()
+}
+
+fun Context.longToast(msg: String) {
+    Toast.makeText(this, msg, Toast.LENGTH_LONG).show()
+}
+
+fun Context.shortIntToast(msg: Int) {
+    Toast.makeText(this, msg, Toast.LENGTH_SHORT).show()
 }
