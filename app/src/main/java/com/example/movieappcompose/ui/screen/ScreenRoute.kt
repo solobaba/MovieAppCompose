47c3049148d41dcf3718df92e5d8ef819969e0b5
Solomon.Oduniyi <solomon.oduniyi@gmail.com>
2024-02-14 17:19:49 +0100

    Bottom bar implementation

--- app/src/main/java/com/example/movieappcompose/ui/screen/ScreenRoute.kt
+++ app/src/main/java/com/example/movieappcompose/ui/screen/ScreenRoute.kt
@@ -1,8 +1,12 @@
 package com.example.movieappcompose.ui.screen
 
+import com.example.movieappcompose.R
+
 const val ID = "id"
 sealed class ScreenRoute(val route: String) {
     object Home: ScreenRoute(route = "moviesHome")
     object Explore: ScreenRoute(route = "explorerMoviesList/{$ID}")
     object Details: ScreenRoute(route = "movieDetails/{movies_id}")
+
+    object Search: ScreenRoute(route = "search")
 }
