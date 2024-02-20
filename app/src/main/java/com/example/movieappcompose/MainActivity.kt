751cfdc20003e75d42a80e8ef84f3de31f92e4a7
Solomon <solomon.oduniyi@gmail.com>
2024-02-20 15:46:43 +0100

    Modify bottom bar naivgation

--- app/src/main/java/com/example/movieappcompose/MainActivity.kt
+++ app/src/main/java/com/example/movieappcompose/MainActivity.kt
@@ -3,13 +3,17 @@ package com.example.movieappcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.max
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.example.movieappcompose.theme.MovieAppComposeTheme
@@ -42,9 +46,19 @@ class MainActivity : ComponentActivity() {
                         }
                     }
                 ) { innerPadding ->
-                    MoviesAppNavHost(
-                        navController,
-                        innerPadding
+                    Box(
+                        modifier = Modifier.padding(
+                            bottom = max(
+                                a = 0.dp,
+                                b = innerPadding.calculateBottomPadding() - 20.dp
+                            ),
+                        ),
+                        content = {
+                            MoviesAppNavHost(
+                                navController,
+                                innerPadding
+                            )
+                        },
                     )
                 }
             }
