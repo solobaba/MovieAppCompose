27ad64d223299e4a05506d73050923491e0653a1
Solomon <solomon.oduniyi@gmail.com>
2024-07-30 20:13:58 +0100

    Clean up build.gradle app module

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,7 +61,6 @@ dependencies {
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
-    //implementation("androidx.compose.material3:material3")
     implementation("androidx.compose.material3:material3-android:1.2.1")
     implementation("androidx.compose.material:material:1.6.8")
 
@@ -103,7 +102,6 @@ dependencies {
     implementation("com.squareup.moshi:moshi-kotlin:1.14.0")
     implementation("com.squareup.retrofit2:converter-moshi:2.9.0")
     implementation("com.squareup.moshi:moshi:1.14.0")
-    //kapt("com.squareup.moshi:moshi-kotlin-codegen:1.14.0")
 
     // Coroutines
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.1")
@@ -113,7 +111,6 @@ dependencies {
     implementation("androidx.room:room-runtime:2.6.0")
     kapt("androidx.room:room-compiler:2.6.1")
     implementation("androidx.room:room-ktx:2.6.1")
-    //ksp("androidx.room:room-compiler:2.4.2")
 
     // Dagger - Hilt
     implementation("com.google.dagger:hilt-android:2.48")
